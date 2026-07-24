5d24d07f0a412df439937df506b6a9019af96b1c
BW <cotsuka@ChienoMacBook-Pro.local>
2026-07-24 12:25:55 +0900

    change:sc15【簡易アプリケーション〜データの更新〜】

--- connection.php
+++ connection.php
@@ -25,3 +25,18 @@ function getAllRecords() //データの取得処理(sc13)
     $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
     return $dbh->query($sql)->fetchAll();
 }
+
+function updateTodoData($post)// 更新処理(sc15)
+{
+    $dbh = connectPdo();
+    $sql = 'UPDATE todos SET content = "' . $post['content'] . '" WHERE id = ' . $post['id'];
+    $dbh->query($sql);
+}
+
+function getTodoTextById($id)// (sc15)
+{
+    $dbh = connectPdo();
+    $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL AND id =' . $id;
+    $data = $dbh->query($sql)->fetch();
+    return $data['content'];
+}
--- functions.php
+++ functions.php
@@ -1,12 +1,32 @@
 <?php
 require_once('connection.php');//データの受け取り処理(sc12)
 
-function createData($post)
-{
-    createTodoData($post['content']);  
-}
-
 function getTodoList()//取得したデータを画面に表示させる(sc13)
 {
     return getAllRecords();
+}
+
+function getSelectedTodo($id)//(sc15)
+{
+    return getTodoTextById($id); 
+}
+function savePostedData($post) //処理の振り分けとリダイレクト設定(sc15)
+{
+    $path = getRefererPath();
+    switch ($path) {
+        case '/new.php':
+            createTodoData($post['content']); //新規作成ページからPOSTされたなら、createTodoData関数 を実行（INSERT処理）
+            break;
+        case '/edit.php':
+            updateTodoData($post); //編集ページからPOSTされたなら、updateTodoData関数 を実行（UPDATE処理）
+            break;
+        default:
+            break;
+    }
+}
+
+function getRefererPath()//処理の振り分けとリダイレクト設定(sc15)
+{
+    $urlArray = parse_url($_SERVER['HTTP_REFERER']);
+    return $urlArray['path'];
 }
